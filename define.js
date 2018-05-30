75c04f9b01f4603212c39f091e134624531bc2ee
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-05-30 17:39:25 +0530

    Added define.js

---
+++ define.js
@@ -0,0 +1,36 @@
+
+var modules = {
+    require: require,
+    exports: {}
+};
+
+function require(name){
+    return modules[name];
+}
+
+var pending = {};
+
+var baseUrl = "";
+
+function define(requires, factory, name){
+    var hasAll = true;
+    for(var i = 0; i < requires.length ; i++){
+        var item = requires[i];
+        if(!modules[item]){
+            hasAll = false;
+            if(!pending[item]){
+                var fx = function(){
+                    define(requires, factory, item);
+                };
+                pending[item] = fx;
+                global.bridge.executeScript(baseUrl, item, fx);
+            }
+        }
+    }
+
+    if(hasAll) {
+        var mExports = {};
+        factory(require,mExports);
+        modules[name || "exports"] = mExports;
+    }
+}
