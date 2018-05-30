66295822d115cd69b1a53adea752137720394cfd
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-05-30 23:22:30 +0530

    wip

--- define.js
+++ define.js
@@ -15,14 +15,14 @@ function define(requires, factory, name){
     for(var i = 0; i < requires.length ; i++){
         var item = requires[i];
         if(!modules[item]){
-            item = global.bridge.resolveName(item);
+            item = bridge.resolveName(item);
             hasAll = false;
             if(!pending[item]){
                 var fx = function(){
                     define(requires, factory, item);
                 };
                 pending[item] = fx;
-                global.bridge.executeScript(item, fx);
+                bridge.executeScript(item, fx);
             }
         }
     }
