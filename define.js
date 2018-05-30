c0605ea2c336c8ca615d7e059c8fa12347312dc7
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-05-31 01:17:24 +0530

    wip

--- define.js
+++ define.js
@@ -3,46 +3,62 @@ var amdLoader = {
     modules: {
         
         require: function(name){
-            return amdLoader.modules[name];
+            return amdLoader.modules[name].exports;
         },
 
         exports: {
 
         }
     },
-    pending: {
-
-    },
+    pending: [],
 };
 
-function define(requires, factory, name){
+function define(requires, factory){
+
+    var currentModule = amdLoader.modules[bridge.baseUrl];
+    if(!currentModule){
+        // seems first..
+        currentModule = {
+            name: bridge.baseUrl,
+            exports: {
+
+            }
+        };
+        amdLoader.modules[bridge.baseUrl] = currentModule;
+    }
+
     var hasAll = true;
     for(var i = 0; i < requires.length ; i++){
         var item = requires[i];
         if(!/^(require|exports)$/.test(item)){
             item = name || bridge.resolveName(item);
         }
-        if(!amdLoader.modules[item]){
+
+        var module = amdLoader.modules[item];
+        if(!module) {
+            module = {
+                name: item,
+                exports: {
+
+                }
+            };
+            amdLoader.modules[item] = module;
+        }
+        if(!module.isLoaded){
             hasAll = false;
-            if(!amdLoader.pending[item]){
-                var fx = function(){
-                    define(requires, factory, item);
-                };
-                amdLoader.pending[item] = fx;
-                bridge.executeScript(item, fx);
-            }
+        }
+        if(!module.isLoading){
+            module.isLoading = true;
+            var fx = function(){
+                bridge.baseUrl = currentModule.name;
+                define(requires,factory);
+            };
+            bridge.executeScript(item, fx);
         }
     }
 
     if(hasAll) {
-        var mExports = {};
-        factory(amdLoader.require, mExports);
-        amdLoader.modules[name || "exports"] = mExports;
-
-        if(!name) {
-            // all modules are loaded...
-            bridge.appLoaded(mExports);
-        }
+        factory(amdLoader.require, currentModule.exports);
     }
 }
 
