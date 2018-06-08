8c8f8842aad69e1b7079c6266880759f123ef8c7
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-06-08 19:11:53 +0530

    Added window service interface

--- src/services/WindowService.ts
+++ src/services/WindowService.ts
@@ -1,9 +1,19 @@
+import { AtomControl } from "../controls/AtomControl";
 import { Service } from "../di";
+import { AtomViewModel } from "../view-model/AtomViewModel";
 
 @Service()
 export class WindowService {
 
     public confirm(message: string, title: string): Promise<any> {
         throw new Error("Method not implemented.");
+    }
+
+    public alert(message: string, title?: string): Promise<any> {
+        throw new Error("Not implemented");
+    }
+
+    public openPopup<T>(c: {new(): AtomControl}, vm: AtomViewModel): Promise<T> {
+        throw new Error("Not implemented");
     }
 }
