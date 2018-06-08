72ecd7079378c896aa48e7e682419b7eb85a28b5
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-06-09 00:45:08 +0530

    All tests successful

--- src/test.ts
+++ src/test.ts
@@ -2,10 +2,10 @@
 import { TestContext, TestRunner } from "./unit/base-test";
 
 // import unit test modules here
-// import "./core/atom-ui-tests";
+import "./core/atom-ui-tests";
 
-// import "./controls/atom-control-tests";
-// import "./controls/atom-items-control-tests";
+import "./controls/atom-control-tests";
+import "./controls/atom-items-control-tests";
 
 import "./di/tests";
 
