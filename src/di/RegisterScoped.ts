22cfa26d2e375ecb3e4fd91a58b63fabcd1f94e5
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-07-02 15:28:31 +0530

    Added support for abstract class registration

--- src/di/RegisterScoped.ts
+++ src/di/RegisterScoped.ts
@@ -2,6 +2,6 @@ import { IClassOf } from "../core/types";
 import { Register } from "./Register";
 import { Scope } from "./ServiceCollection";
 
-export function RegisterScoped(id: IClassOf<any>): any {
+export function RegisterScoped(id: any): any {
     Register({scope: Scope.Scoped})(id);
 }
--- src/di/RegisterSingleton.ts
+++ src/di/RegisterSingleton.ts
@@ -2,6 +2,6 @@ import { IClassOf } from "../core/types";
 import { Register } from "./Register";
 import { Scope } from "./ServiceCollection";
 
-export function RegisterSingleton(target: IClassOf<any>): void {
+export function RegisterSingleton(target: any): void {
     Register({scope: Scope.Global})(target);
 }
