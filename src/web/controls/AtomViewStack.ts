d935e99feb531bfb1081d56c9ff2922ee55e13e4
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-07-19 19:48:56 +0530

    View Stack fixed

--- src/web/controls/AtomViewStack.ts
+++ src/web/controls/AtomViewStack.ts
@@ -14,27 +14,19 @@ export class AtomViewStack extends AtomControl {
         const ee = e instanceof AtomControl ? (e as AtomControl).element : e as HTMLElement;
         ((ee as any) as IAtomControlElement)._logicalParent = this.element as IAtomControlElement;
         this.children = this.children || [];
+        const index = this.children.length;
         this.children.push(e instanceof AtomControl ? (e as AtomControl).element : e as HTMLElement);
         if (this.selectedIndex === undefined) {
             this.selectedIndex = 0;
         }
+
+        const style = ee.style;
+        style.position = "absolulte";
+        style.top = style.left = style.right = style.bottom = "0";
+
+        this.bind(ee, "styleVisibility", [["selectedIndex"]], false, (v) => v === index ? "visibile" : "hidden" );
+
         return this;
-    }
-
-    public onUpdateUI(): void {
-        const e = this.children[this.selectedIndex];
-        if (!e) {
-            return;
-        }
-
-        if (this.current === e) {
-            super.onUpdateUI();
-            return;
-        }
-
-        this.current.remove();
-        this.element.appendChild(e);
-        this.current = e;
     }
 
 }
