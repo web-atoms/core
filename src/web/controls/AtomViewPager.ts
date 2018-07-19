301c449117e7757187c6c522208237105fb680e2
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-07-19 12:40:15 +0530

    AtomViewPager size updated

--- src/web/controls/AtomViewPager.ts
+++ src/web/controls/AtomViewPager.ts
@@ -27,6 +27,10 @@ export class AtomViewPager extends AtomItemsControl {
     protected create(): void {
         super.create();
 
+        const estyle = this.element.style;
+        estyle.position = "absolute";
+        estyle.left = estyle.right = estyle.bottom = estyle.top = "0";
+
         const cc = new AtomContentControl(this.app);
         this.append(cc);
         const style = cc.element.style;
