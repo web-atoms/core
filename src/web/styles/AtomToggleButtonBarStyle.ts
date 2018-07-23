9d1bd009220d933120cb91316419b6ee5c1ca03a
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-07-24 00:47:52 +0530

    Added ToggleButtonBarStyle

--- src/web/styles/AtomToggleButtonBarStyle.ts
+++ src/web/styles/AtomToggleButtonBarStyle.ts
@@ -13,13 +13,13 @@ export class AtomToggleButtonBarStyle extends AtomListBoxStyle {
             borderRadius: 0,
             display: "inline-block"
         }));
-        this.item.subClass("> *:first-child", () => ({
+        this.item.subClass(":first-child", () => ({
             borderTopLeftRadius: `${this.padding}px`,
             borderBottomLeftRadius: `${this.padding}px`,
             borderTopRightRadius: 0,
             borderBottomRightRadius: 0
         }));
-        this.selectedItem.subClass("> *:last-child", () => ({
+        this.selectedItem.subClass(":last-child", () => ({
             borderTopLeftRadius: 0,
             borderBottomLeftRadius: 0,
             borderTopRightRadius: `${this.padding}px`,
