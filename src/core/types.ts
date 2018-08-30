db8956f16ba9b1ae7cc462544e642fa5785ce1f3
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-08-30 15:15:34 -0400

    Popup fix

--- src/core/types.ts
+++ src/core/types.ts
@@ -129,3 +129,15 @@ export declare class UMD {
 }
 
 export const DI = UMD;
+
+export function DIMock(name: string): any {
+    return (target: any): void => {
+        UMD.mockType(target, name);
+    };
+}
+
+export function DIInject(name: string): any {
+    return (target: any): void => {
+        UMD.inject(target, name);
+    };
+}
