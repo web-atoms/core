0d9cdf2651417c4b055be683daa4bc31fb37c09f
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-10-08 17:06:38 +0530

    wip

--- src/web/styles/AtomTabbedPageStyle.ts
+++ src/web/styles/AtomTabbedPageStyle.ts
@@ -1,8 +1,9 @@
 import { BindableProperty } from "../../core/BindableProperty";
 import { ModuleFiles } from "../../ModuleFiles";
 import { AtomStyle } from "./AtomStyle";
-import { AtomStyleClass } from "./AtomStyleClass";
+import { AtomStyleClass, StyleClass } from "./AtomStyleClass";
 import { AtomTheme } from "./AtomTheme";
+import { IStyleDeclaration } from "./IStyleDeclaration";
 
 export class AtomTabbedPageStyle extends AtomStyle {
 
@@ -13,58 +14,73 @@ export class AtomTabbedPageStyle extends AtomStyle {
         return this.styleSheet as AtomTheme;
     }
 
-    public readonly tabItem: AtomStyleClass = this.createClass("tab-item", () => ({
-        display: "inline-block",
-        borderTopLeftRadius: (this.padding || this.theme.padding) + "px",
-        borderTopRightRadius: (this.padding || this.theme.padding) + "px",
-        marginLeft: "2px",
-        padding: 0,
-        borderStyle: "solid",
-        borderWidth: "1px",
-        borderColor: "lightgray",
-        backgroundColor: this.theme.bgColor,
-        minWidth: "90px",
-        minHeight: "30px",
-        cursor: "default",
-        position: "relative"
-    }))
-    .hover(() => ({
-        backgroundColor: this.theme.hoverColor
-    }))
-    .immediateChild("div", () => ({
-        display: "inline-block",
-        padding: (this.padding || this.theme.padding) + "px",
-        paddingRight: ((this.padding || this.theme.padding) + 23) + "px",
-        right: "22px"
-    }));
+    @StyleClass("tab-item")
+    public get tabItem(): IStyleDeclaration {
+        return {
+            display: "inline-block",
+            borderTopLeftRadius: (this.padding || this.theme.padding) + "px",
+            borderTopRightRadius: (this.padding || this.theme.padding) + "px",
+            marginLeft: "2px",
+            padding: 0,
+            borderStyle: "solid",
+            borderWidth: "1px",
+            borderColor: "lightgray",
+            backgroundColor: this.theme.bgColor,
+            minWidth: "90px",
+            minHeight: "30px",
+            cursor: "default",
+            position: "relative",
+            subclasses: {
+                ":hover": {
+                    backgroundColor: this.theme.hoverColor
+                },
+                "> div": {
+                    display: "inline-block",
+                    padding: (this.padding || this.theme.padding) + "px",
+                    paddingRight: ((this.padding || this.theme.padding) + 23) + "px",
+                    right: "22px"
+                }
+            }
+        };
+    }
 
-    public readonly selectedTabItem = this.tabItem.clone("selected-tab-item", () => ({
-        borderColor: this.theme.activeColor,
-        backgroundColor: this.theme.selectedBgColor,
-        color: this.theme.selectedColor
-    }))
-    .hover(() => ({
-        backgroundColor: this.theme.selectedBgColor,
-        color: this.theme.hoverColor
-    }))
-    .immediateChild("div", () => ({
-        display: "inline-block",
-        padding: (this.padding || this.theme.padding) + "px",
-        paddingRight: ((this.padding || this.theme.padding) + 23) + "px",
-        right: "22px"
-    }));
+    @StyleClass("selected-tab-item")
+    public get selectedTabItem(): IStyleDeclaration {
+        return {
+            borderColor: this.theme.activeColor,
+            backgroundColor: this.theme.selectedBgColor,
+            color: this.theme.selectedColor,
+            subclasses: {
+                ":hover": {
+                    backgroundColor: this.theme.selectedBgColor,
+                    color: this.theme.hoverColor
+                },
+                "> div": {
+                    display: "inline-block",
+                    padding: (this.padding || this.theme.padding) + "px",
+                    paddingRight: ((this.padding || this.theme.padding) + 23) + "px",
+                    right: "22px"
+                }
+            }
+        };
+    }
 
-    public readonly closeButton = this.createClass("close-button", () => ({
-        position: "absolute",
-        right: "5px",
-        top: "5px",
-        width: "0",
-        height: "0",
-        padding: "8px",
-        backgroundImage: `url(${ModuleFiles.src.web.images.closeButton_svg})`
-    }))
-    .hover(() => ({
-        backgroundImage: `url(${ModuleFiles.src.web.images.closeButtonHover_svg})`
-    }));
+    @StyleClass("close-button")
+    public get closeButton(): IStyleDeclaration {
+        return {
+            position: "absolute",
+            right: "5px",
+            top: "5px",
+            width: "0",
+            height: "0",
+            padding: "8px",
+            backgroundImage: `url(${ModuleFiles.src.web.images.closeButton_svg})`,
+            subclasses: {
+                ":hover": {
+                    backgroundImage: `url(${ModuleFiles.src.web.images.closeButtonHover_svg})`
+                }
+            }
+        };
+    }
 
 }
