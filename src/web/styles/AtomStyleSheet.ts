f416c3853f54bb3a619a1b1af03fac715aea3cc8
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-10-11 23:42:39 +0530

    Tests fixed

--- src/web/styles/AtomStyleSheet.ts
+++ src/web/styles/AtomStyleSheet.ts
@@ -8,8 +8,6 @@ export class AtomStyleSheet extends AtomStyle
 
     private isAttaching: boolean = false;
 
-    private isAttaching: boolean = false;
-
     [key: string]: any;
 
     constructor(prefix: string) {
