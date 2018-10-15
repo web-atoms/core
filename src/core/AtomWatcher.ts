0d6279ea49a93776bb73f756973d29ef9daa48ec
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-10-15 14:37:35 +0530

    Removed watch expression

--- src/core/AtomWatcher.ts
+++ src/core/AtomWatcher.ts
@@ -290,7 +290,7 @@ export class AtomWatcher<T> implements IDisposable {
             }
         }
         // tslint:disable-next-line:no-string-literal
-        this["disposedPath"] = this.path;
+        // this["disposedPath"] = this.path;
         this.func = null;
         // this.path.length = 0;
         this.path = null;
