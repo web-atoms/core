529e1d142a1fc0f3fc1c9b9e3e111cdb1eb71010
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-10-17 20:40:22 +0530

    Added few tests

---
+++ src/tests/web/controls/AtomWindowTest.ts
@@ -0,0 +1,43 @@
+import { Atom } from "../../../Atom";
+import { AtomTest } from "../../../unit/AtomTest";
+import { Test } from "../../../unit/Test";
+import { AtomControl } from "../../../web/controls/AtomControl";
+import { AtomWindow } from "../../../web/controls/AtomWindow";
+import AtomWebTest from "../AtomWebTest";
+
+export class AtomWindowTest extends AtomWebTest {
+
+    @Test
+    public async drag(): Promise<any> {
+
+        const win = new SampleWindow(this.app);
+
+        await Atom.delay(100);
+
+        // window.dispatchEvent(new MouseEvent("mousedown"));
+        // window.dispatchEvent(new MouseEvent("mousemove"));
+        // window.dispatchEvent(new MouseEvent("mouseup"));
+
+        win.dispose();
+
+    }
+
+}
+
+class SampleWindow extends AtomWindow {
+
+    protected create(): void {
+        this.element = document.createElement("div");
+
+        this.windowTemplate = SampleWindowTemplate;
+    }
+
+}
+
+class SampleWindowTemplate extends AtomControl {
+
+    protected create(): void {
+        this.element = document.createElement("div");
+        this.element.textContent = "sample";
+    }
+}
--- src/tests/web/window/WindowTest.ts
+++ src/tests/web/window/WindowTest.ts
@@ -1,11 +1,96 @@
+import { MockNavigationService } from "../../../services/MockNavigationService";
+import { NavigationService } from "../../../services/NavigationService";
+import { Assert } from "../../../unit/Assert";
 import { Test } from "../../../unit/Test";
 import { TestItem } from "../../../unit/TestItem";
+import AtomWebTest from "../AtomWebTest";
 
-export class TestCase extends TestItem {
+export class TestCase extends AtomWebTest {
 
     @Test
-    public async cancelWindow(): Promise<any> {
+    public async noWindowRegistered(): Promise<any> {
+        const nav = this.navigationService;
+
+        try {
+            await nav.openWindow("WinA");
+            Assert.throw("Should not reach here");
+        } catch (e) {
+            Assert.isTrue(/no window registered for/i.test(e));
+        }
+    }
+
+    @Test
+    public async removeExpectedWindow(): Promise<any> {
+        const nav = this.navigationService;
+
+        const d = nav.expectWindow("WinA", (v) => null);
+
+        d.dispose();
+
+        await this.noWindowRegistered();
+    }
+
+    @Test
+    public async confirmTest(): Promise<any> {
+        const nav = this.navigationService;
+
+        nav.expectConfirm("C1", (mv) => true);
+
+        Assert.isTrue( await nav.confirm("C1") );
+
+        nav.expectConfirm("C2", (vm) => false);
+
+        Assert.isFalse( await nav.confirm("C2"));
+    }
+
+    @Test
+    public async openWindowError(): Promise<any> {
+        const nav = this.navigationService;
+
+        nav.expectWindow("WinA", (vm) => {
+            throw new Error("Error");
+        });
+
+        try {
+            await nav.openWindow("WinA");
+            Assert.throw("Should not reach here");
+        } catch (e) {
+            Assert.isTrue(e.message === "Error");
+        }
+    }
+
+    @Test
+    public async expectFailure(): Promise<any> {
+        const nav = this.navigationService;
+
+        nav.expectAlert("Hello");
+
+        try {
+            nav.assert();
+            Assert.throw("Should not reach here");
+        } catch (e) {
+            Assert.isTrue(/expected windows did not open/i.test(e));
+        }
+
+        await nav.alert("Hello");
+    }
+
+    @Test
+    public async expectWindow(): Promise<any> {
         // task: pending
+
+        const nav = this.navigationService;
+
+        nav.expectWindow("WinA", (vm) => {
+            return "result";
+        });
+
+        const result = await nav.openPage("WinA");
+
+        Assert.equals("result", result);
+
+        nav.assert();
+
     }
 
 }
