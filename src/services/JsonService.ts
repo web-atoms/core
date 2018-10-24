6631b5e69a2be98122dbc95b847c8f23b45ef3a8
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-10-25 00:47:51 +0530

    Added json options

--- src/services/JsonService.ts
+++ src/services/JsonService.ts
@@ -1,18 +1,49 @@
+import { IValueConverter } from "../core/IValueConverter";
 import { StringHelper } from "../core/StringHelper";
 import { RegisterSingleton } from "../di/RegisterSingleton";
 
-const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
-const reMsAjax = /^\/Date\((d|-|.*)\)[\/|\\]$/;
+export const dateFormatISORegEx = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+export const dateFormatMSRegEx = /^\/Date\((d|-|.*)\)[\/|\\]$/;
 
 export type JsonKeysNamingStrategy = "underscore" | "hyphen" | "none";
+
+export interface IJsonParserOptions {
+    namingStrategy?: JsonKeysNamingStrategy;
+    dateConverter?: Array<{
+        regex?: RegExp;
+        valueCovnerter: IValueConverter;
+    }>;
+    indent?: number;
+}
 
 @RegisterSingleton
 export class JsonService {
 
-    /**
-     * When set to none, no key transform is performed
-     */
-    public namingStrategy: JsonKeysNamingStrategy = "none";
+    public options: IJsonParserOptions = {
+        indent: 2,
+        namingStrategy: "none",
+        dateConverter: [
+            {
+                regex: dateFormatISORegEx,
+                valueCovnerter: {
+                    fromSource(v: string): Date {
+                        return new Date(v);
+                    },
+                    fromTarget: null
+                }
+            }, {
+                regex: dateFormatMSRegEx,
+                valueCovnerter: {
+                    fromSource(v: string): Date {
+                        const a = dateFormatMSRegEx.exec(v);
+                        const b = a[1].split(/[-+,.]/);
+                        return new Date(b[0] ? +b[0] : 0 - +b[1]);
+                    },
+                    fromTarget: null
+                }
+            }
+        ]
+    };
 
     public transformKeys(t: (ins: string) => string, v: any): any {
         if (!v) {
@@ -43,23 +74,26 @@ export class JsonService {
         return r;
     }
 
-    public parse(text: string): any {
+    public parse(text: string, options?: IJsonParserOptions): any {
+
+        const { dateConverter, namingStrategy } = {
+            ... this.options,
+            ... options
+        };
+
         const result = JSON.parse(text, (key, value) => {
             // trannsform date...
             if (typeof value === "string") {
-                let a = reISO.exec(value);
-                if (a) {
-                    return new Date(value);
-                }
-                a = reMsAjax.exec(value);
-                if (a) {
-                    const b = a[1].split(/[-+,.]/);
-                    return new Date(b[0] ? +b[0] : 0 - +b[1]);
+                for (const iterator of dateConverter) {
+                    const a = iterator.regex.exec(value);
+                    if (a) {
+                        return iterator.valueCovnerter.fromSource(value);
+                    }
                 }
             }
             return value;
         } );
-        switch (this.namingStrategy) {
+        switch (namingStrategy) {
             case "hyphen":
                 return this.transformKeys(StringHelper.fromHyphenToCamel, result);
             case "underscore":
@@ -68,8 +102,12 @@ export class JsonService {
         return result;
     }
 
-    public stringify(v: any): string {
-        switch (this.namingStrategy) {
+    public stringify(v: any, options?: IJsonParserOptions): string {
+        const { namingStrategy, dateConverter, indent } = {
+            ... this.options,
+            ... options
+        };
+        switch (namingStrategy) {
             case "hyphen":
                 v = this.transformKeys(StringHelper.fromCamelToHyphen, v);
                 break;
@@ -78,10 +116,13 @@ export class JsonService {
                 break;
         }
         return JSON.stringify(v, (key, value) => {
-            if (/^\_\$\_/.test(key)) {
+            if (key && /^\_\$\_/.test(key)) {
                 return undefined;
             }
+            // if (dateConverter && (value instanceof Date)) {
+            //     return dateConverter[0].valueCovnerter.fromTarget(value);
+            // }
             return value;
-        }, 2);
+        }, indent);
     }
 }
