5073e3b436aeab765a4bbdb80e48dd9ff138d857
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-11-06 18:14:08 +0530

    wip

--- src/core/AtomSelectableList.ts
+++ src/core/AtomSelectableList.ts
@@ -1,8 +1,9 @@
 import { AtomViewModel } from "../view-model/AtomViewModel";
+import { AtomBinder } from "./AtomBinder";
 import { AtomDisposableList } from "./AtomDisposableList";
 import { IDisposable } from "./types";
 
-export type valuePathOrFunc<T> = string | ((item: T) => any);
+export type valuePathOrFunc<T> = ((item: T) => any);
 
 export default class AtomSelectableList<T> {
 
@@ -10,14 +11,44 @@ export default class AtomSelectableList<T> {
 
     public readonly selectedItems: Array<ISelectableItem<T>> = [];
 
+    public get selectedIndex(): number {
+        if (this.selectedItems.length) {
+            return this.items.indexOf(this.selectedItems[0]);
+        }
+        return -1;
+    }
+
+    public set selectedIndex(n: number) {
+        this.selectedItems.clear();
+        if (n === -1) {
+            return;
+        }
+        this.selectedItems.add(this.items[n]);
+    }
+
     private mValue: any = undefined;
     public get value(): any {
-        if (this.mValue !== undefined) {
-            return this.mValue;
+        if (this.selectedItems.length) {
+            if (this.allowMultipleSelection) {
+                return this.selectedItems.map((x) => this.valuePath(x.item));
+            }
+            return this.valuePath(this.selectedItems[0].item);
         }
+        return this.mValue;
     }
+
     public set value(v: any) {
         this.mValue = v;
+        if (!this.allowMultipleSelection) {
+            v = [v];
+        }
+        const va = v as any;
+        const values = this.items.filter((x) => {
+            const vp = this.valuePath(x.item);
+            const existing = va.find((y) => y === vp );
+            return existing ? true : false;
+        });
+        this.selectedItems.replace(values);
     }
 
     constructor(
@@ -38,6 +69,9 @@ export default class AtomSelectableList<T> {
             }
         }
         this.items = source.map((x) => this.newItem(x));
+        if (!this.valuePath) {
+            this.valuePath = (x) => x ? x.toString() : undefined;
+        }
     }
 
     private syncItems(target: T[], key: string, index: number, a: T): void {
@@ -59,7 +93,7 @@ export default class AtomSelectableList<T> {
 
     private newItem(item: T): ISelectableItem<T> {
         const self = this;
-        return {
+        const newItem = {
             item,
             get selected(): boolean {
                 return self.selectedItems.find((x) => x.item === item) ? true : false;
@@ -69,12 +103,25 @@ export default class AtomSelectableList<T> {
                     if (this.selected) {
                         return;
                     }
+                    if (!self.allowMultipleSelection) {
+                        self.selectedItems.clear();
+                    }
                     self.selectedItems.add(this);
                 } else {
                     self.selectedItems.remove(this);
                 }
+                AtomBinder.refreshValue(self, "value");
             }
         };
+        const value = this.valuePath(item);
+        let values = this.value as any[];
+        if (!this.allowMultipleSelection) {
+            values = [values];
+        }
+        if (values.find((x) => x === value)) {
+            newItem.selected = true;
+        }
+        return newItem;
     }
 
 }
