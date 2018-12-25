ee247b40a407e0a8a0ea9d8efcf742bb0aa14299
Akash Kava <39438041+ackava@users.noreply.github.com>
2018-12-25 10:47:59 +0530

    Added map support

--- src/di/TypeKey.ts
+++ src/di/TypeKey.ts
@@ -6,10 +6,27 @@ export class TypeKey {
                 return iterator.key;
             }
         }
-        const key = `key${this.keys.length}`;
+        const key = `${c.name || "key"}${this.keys.length}`;
         this.keys.push({ c, key});
         return key;
     }
 
     private static keys: Array<{c: any, key: string}> = [];
 }
+
+// if (Map !== undefined) {
+
+//     const map = new Map<any, string>();
+
+//     const oldGet = TypeKey.get;
+
+//     TypeKey.get = (c: any): string => {
+//         const v = map.get(c);
+//         if (!v) {
+//             return v;
+//         }
+//         const v1 = oldGet(c);
+//         map.set(c, v1);
+//         return v1;
+//     };
+// }
