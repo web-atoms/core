8a9f84ad60e769f29cdf9aae8fadb236ad76454e
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-08-01 19:01:51 +0530

    wip

--- src/web/controls/AtomListBox.ts
+++ src/web/controls/AtomListBox.ts
@@ -11,15 +11,32 @@ export class AtomListBox extends AtomItemsControl {
     protected preCreate(): void {
         super.preCreate();
         this.defaultControlStyle = AtomListBoxStyle;
+
+        this.bindEvent(this.element, "click", (e) => {
+            const p = this.atomParent(e.target as HTMLElement);
+            if (p === this) {
+                return;
+            }
+            if (p.element._logicalParent === this.element) {
+                // this is child..
+                const data = p.data;
+                if (!data) {
+                    return;
+                }
+                if (this.selectItemOnClick) {
+                    this.toggleSelection(data);
+                    const ce = new CustomEvent("selectionChanged", {
+                        bubbles: false,
+                        cancelable: false,
+                        detail: data });
+                    this.element.dispatchEvent(ce);
+                }
+            }
+        });
     }
 
     protected createChild(df: DocumentFragment, data: any): AtomControl {
         const child = super.createChild(df, data);
-        child.bindEvent(child.element, "click", (e) => {
-            if (this.selectItemOnClick) {
-                this.toggleSelection(data);
-            }
-        });
         child.bind(child.element, "styleClass",
             [
                 ["this", "version"],
