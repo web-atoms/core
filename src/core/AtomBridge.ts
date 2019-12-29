6bc81b0af8350ff9934bdfcb1509b5582ea19f2a
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-12-29 15:43:24 +0530

    Renderer fixed

--- src/core/AtomBridge.ts
+++ src/core/AtomBridge.ts
@@ -227,14 +227,25 @@ export class AtomElementBridge extends BaseElementBridge<HTMLElement> {
         // tslint:disable-next-line: ban-types
         creator: Function): any {
 
+        let parent = null;
+
         const app = target.app;
         let e: HTMLElement = null;
         if (typeof node.name === "string") {
             // it is simple node..
             e = document.createElement(node.name);
+            parent = e;
         } else {
             target = new (node.name as any)(app);
             e = target.element;
+            parent = target;
+            // target.append(child);
+            // const firstChild = node.children ? node.children[0] : null;
+            // if (firstChild) {
+            // 	const n = this.createNode(child, firstChild, binder, xNodeClass, creator);
+            // 	child.append(n.atomControl || n);
+            // }
+            // return child.element;
         }
 
         const a = node.attributes;
@@ -243,7 +254,17 @@ export class AtomElementBridge extends BaseElementBridge<HTMLElement> {
                 if (a.hasOwnProperty(key)) {
                     let element = a[key] as any;
                     if (element instanceof binder) {
-                        (element as any).setupFunction(key, element, target, e);
+                        if (/^event/.test(key)) {
+                            let ev = key.substr(5);
+                            if (ev.startsWith("-")) {
+                                ev = ev.split("-").map((s) => s[0].toLowerCase() + s.substr(1)).join("");
+                            } else {
+                                ev = ev[0].toLowerCase() + ev.substr(1);
+                            }
+                            (element as any).setupFunction(ev, element, target, e);
+                        } else {
+                            (element as any).setupFunction(key, element, target, e);
+                        }
                     } else {
 
                         // this is template...
@@ -279,11 +300,19 @@ export class AtomElementBridge extends BaseElementBridge<HTMLElement> {
         if (children) {
             for (const iterator of children) {
                 if (typeof iterator === "string") {
-                    e.append(document.createTextNode(iterator));
+                    e.appendChild(document.createTextNode(iterator));
                     continue;
                 }
-                const child = this.createNode(target, iterator, binder, xNodeClass, creator);
-                e.append(child);
+                if (typeof iterator.name === "string") {
+                    e.appendChild(this.createNode(target, iterator, binder, xNodeClass, creator));
+                } else {
+                    const child = this.createNode(target, iterator, binder, xNodeClass, creator);
+                    if (parent instanceof AtomControl) {
+                        parent.append(child.atomControl || child);
+                    } else {
+                        parent.appendChild(child);
+                    }
+                }
             }
         }
         return e;
