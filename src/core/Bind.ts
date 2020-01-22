d8835a7fef9e142c3f4c6945d017fc472efa23ea
simmi <simmi@neuro>
2020-01-22 14:13:59 +0530

    wip

--- src/core/Bind.ts
+++ src/core/Bind.ts
@@ -64,7 +64,10 @@ function twoWays(name: string, b: Bind, control: IAtomComponent, e: any, creator
 function presenter(name: string, b: Bind, control: IAtomComponent, e: any) {
     const n = b.name || name;
     let c = control.element as any;
-    while (c && c.atomControl && c.atomControl[n] === undefined) {
+    while (c) {
+        if (c.atomControl && c.atomControl[n] !== undefined) {
+            break;
+        }
         c = c._logicalParent || c.parentElement;
     }
     ((c && c.atomControl) || control)[n] = e;
