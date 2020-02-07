88e19de277c1dc20b565e4a4fd951112240af697
Akash Kava <ackava@gmail.com>
2020-02-07 15:32:34 +0530

    Added message subscription

--- src/xf/XFApp.ts
+++ src/xf/XFApp.ts
@@ -26,6 +26,22 @@ export default class XFApp extends A.App {
         bridge.controlFactory = AtomXFControl;
         this.put(NavigationService, this.resolve(XFNavigationService));
         this.put(BusyIndicatorService, this.resolve(XFBusyIndicatorService));
+
+        const s = bridge.subscribe((m) => {
+            this.broadcast(m.channel, m.data);
+        });
+
+        // register for messaging...
+        const oldDispose = this.dispose;
+        this.dispose = () => {
+            s.dispose();
+            oldDispose.call(this);
+        };
+    }
+
+    public broadcast(channel: string, data: any) {
+        super.broadcast(channel, data);
+        bridge.broadcast(channel, data);
     }
 
 }
