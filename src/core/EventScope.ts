99ff96e5372e6d85c48986a8d5759fb9f26f59f4
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-17 19:17:25 +0530

    Added bind event and event bubbling

--- src/core/EventScope.ts
+++ src/core/EventScope.ts
@@ -10,12 +10,23 @@ export default class EventScope<T = any> {
         return new EventScope<T1>(`eventScopeE${key}${id++}`)
     }
 
-    private constructor(public readonly eventType: string) {
+    public bindEvent(fx: (x: CustomEvent<T>) => any) {
+        return {
+            [this.eventName]: fx
+        };
+    }
 
+    public readonly eventName: string;
+
+    private constructor(public readonly eventType: string) {
+        this.eventName = `event-${this.eventType}`;
     }
 
     public listen(fx: (ce: CustomEvent<T>) => any) {
         const asyncHandler = (ce: CustomEvent<T>) => {
+            if (ce.defaultPrevented) {
+                return;
+            }
             try {
                 const p = fx(ce);
                 if (p?.catch) {
@@ -41,7 +52,46 @@ export default class EventScope<T = any> {
         };
     }
 
-    public dispatchEvent(detail: T, cancelable: boolean = false) {
+    public listenOn(target: EventTarget, fx: (ce: CustomEvent<T>) => any) {
+        const asyncHandler = (ce: CustomEvent<T>) => {
+            if (ce.defaultPrevented) {
+                return;
+            }
+            try {
+                const p = fx(ce);
+                if (p?.catch) {
+                    p.catch((r) => {
+                        if(CancelToken.isCancelled(r)) {
+                            return;
+                        }
+                        console.error(r);
+                    });
+                }
+            } catch (e) {
+                if (CancelToken.isCancelled(e)) {
+                    return;
+                }
+                console.error(e);
+            }
+        };
+        target.addEventListener(this.eventType, asyncHandler);
+        return {
+            dispose:() => {
+                target.removeEventListener(this.eventType, asyncHandler);
+            }
+        };
+    }
+
+    public dispatch(target: EventTarget, detail: T, {
+        cancelable = true,
+        bubbles = true
+    } : { cancelable?: boolean, bubbles?: boolean } = {}) {
+        const ce = new CustomEvent(this.eventType, { detail, cancelable, bubbles });
+        target.dispatchEvent(ce);
+        return ce;
+    }
+
+    public dispatchEvent(detail: T, cancelable: boolean = true) {
         const ce = new CustomEvent(this.eventType, { detail, cancelable, bubbles: false });
         window.dispatchEvent(ce);
         return ce;
