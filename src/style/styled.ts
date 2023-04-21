3fc5e96378c43b52b0cc6fcacb5b2f4a766548e3
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-04-21 10:44:12 +0530

    Added styled SASS Support

--- src/style/styled.ts
+++ src/style/styled.ts
@@ -1,57 +1,101 @@
+function *divide (text: string) {
+    const regex = /^(([^\{\n]+\{)|([^\n]*\}))$/gm;
+    let m;
+    let sentOnce = false;
+    let lastIndex = 0;
+    let lastMatch;
+    while((m = regex.exec(text)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        const match = m[0];
+        if(!sentOnce) {
+            // send first text ..
+            // send current ..
+            sentOnce = true;
+            yield text.substring(lastIndex, m.index);
+            lastIndex = m.index + match.length;
+            lastMatch = match;
+            continue;
+        }
+
+        if (lastMatch.endsWith("}")) {
+            yield [lastMatch];
+            lastIndex = m.index + match.length;
+            lastMatch = match;
+            continue;
+        }
+
+        yield [lastMatch, text.substring(lastIndex, m.index), match];
+        lastIndex = m.index + match.length;
+        lastMatch = match;
+    }
+
+    if(lastMatch?.endsWith("}")) {
+        yield [lastMatch];
+    }
+};
+
+
 let id = 1;
 
 const nextId = () => `styled-r${id++}`;
 
 class StyleFragment {
 
-    static newStyle( { selector = "", content = "", children = void 0}) {
-        return new StyleFragment( { selector, content, children })
+    static newStyle( { selector = "", content = ""}) {
+        return new StyleFragment( { selector, content })
     }
 
     private selector: string;
     private content: string;
-    private children: StyleFragment[];
 
-    constructor({ selector, content, children }) {
+    constructor({ selector, content }) {
         this.selector = selector;
         this.content = content;
-        this.children = children;
     }
 
     expand(selector?) {
         selector ??= this.selector;
-        let content = `${selector} {${this.content}}`;
-        if (this.children) {
-            for (const iterator of this.children) {
-                content += "\n";
-                content += iterator.expand(selector + iterator.selector);
-            }
+
+        let en = divide(this.content);
+
+        let parts = en.next();
+
+        if (parts.done) {
+            return `${selector} {\n${this.content}\n}`;
         }
+
+        const first = parts.value;
+
+        let content = `${selector} {\n${first}\n}\n`;
+
+        let selectorStack = [];
+
+        while (!(parts = en.next()).done) {
+            const [key, value] = parts.value as string[];
+
+            if (key.endsWith("}")) {
+                selector = selectorStack.pop();
+                continue;
+            }
+
+            let keySelector = key.replace("{", "");
+
+            const replaced = keySelector.replace(/\&/g, selector).trim();
+            // push stack...
+            selectorStack.push(selector);
+            selector = replaced;                
+
+            content += `${replaced} {\n${value}\n}\n`;
+        }
+
         return content;
     }
 
     toString() {
         return this.expand();
-    }
-
-    and (selector, sf ) {
-        sf.selector = selector;
-        this.children ??= [];
-        this.children.push(sf);
-        return this;
-    }
-
-    child (selector, sf ) {
-        sf.selector = " > " + selector;
-        this.children ??= [];
-        this.children.push(sf);
-        return this;
-    }
-    nested (selector, sf ) {
-        sf.selector = " " + selector;
-        this.children ??= [];
-        this.children.push(sf);
-        return this;
     }
 
     installGlobal(selector: string = "") {
