3e1bbfc89c130c888da47680604181d5988f12ad
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-08-11 16:35:55 +0530

    Added null check

--- src/core/AtomLoader.ts
+++ src/core/AtomLoader.ts
@@ -151,7 +151,7 @@ export class AtomLoader {
         app: App): Promise<T> {
 
         const busyIndicator = app.createBusyIndicator({
-            title: url.toString(),
+            title: url?.toString(),
             description: `Loading View ${url}`
         });
 
