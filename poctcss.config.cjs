6780ff4103a9bce4f72eed7d623b45644e7b4f43
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-04-04 19:40:09 +0530

    Added stage-4

--- poctcss.config.cjs
+++ poctcss.config.cjs
@@ -1,11 +1,14 @@
 module.exports = (ctx) => ({
     map: { ... ctx.options.map, sourcesContent: false },
     plugins: [
-        require("@web-atoms/postcss-styled-js")(),
-        require('postcss-preset-env')(),
-        require("postcss-import")(),
+        require("postcss-import-styled-js")(),
+        require('postcss-preset-env')({
+          stage: 4
+        }),
         require("postcss-import-ext-glob")(),
+        require("postcss-import")(),
         require("postcss-nested")(),
+        require("postcss-copy-assets")({ base: ctx.options.base }),
         require("cssnano")()
     ]
   });
