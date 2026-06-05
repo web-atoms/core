25a38b739e70f77057a21fdef7f07fd430263d99
Akash Kava <ackava@gmail.com>
2026-06-05 20:17:05 +0530

    prioritise control

--- src/di/Inject.ts
+++ src/di/Inject.ts
@@ -1,4 +1,4 @@
-import { InjectedTypes } from "./InjectedTypes.js";
+import { InjectedTypes, serviceInstance } from "./InjectedTypes.js";
 import { ServiceProvider } from "./ServiceProvider.js";
 
 export interface IPropertyTypes {
@@ -24,7 +24,7 @@ export function Inject(target: any, name: string, index?: number): void {
         Object.defineProperty(target, key, {
             get: function() {
                 const plist = (Reflect as any).getMetadata("design:type", target, key);
-                const result = ServiceProvider.from(this.app ?? this).resolve(plist);
+                const result = (this.app || this[serviceInstance]).resolve(plist);
                 // get is compatible with AtomWatcher
                 // as it will ignore getter and it will
                 // not try to set a binding refresher
