d6f782ab7a12bae65243307c267f44eb904ade8d
Akash Kava <ackava@gmail.com>
2026-06-05 19:26:56 +0530

    added inject

---
+++ src/core/InjectProperty.ts
@@ -0,0 +1,7 @@
+import { Inject } from "../di/Inject";
+
+ /** @deprecated use `@Inject` instead */
+export default function InjectProperty(target, name) {
+    console.warn("InjectProperty is deprecated");
+    return Inject(target, name)
+}
